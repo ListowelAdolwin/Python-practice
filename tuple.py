f2f56722f1d6b08a2a40b1f549359621ef8ee310
ListowelAdolwin <listoweladolwin>
2022-06-29 13:30:30 +0000

    A simple python tuple with unpacking concept

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# Tuples are are group of items that cannot be modofied
+# Tuples are represented in braces
+# Unpacking in tuples in where the values of a tuple 
+# stored in different variables in just one line
+
+
+Coordinates = (3, 5, 6) # A tuple of three values
+x, y, z = Coordinates # Unpacking the coordinates and assigning the value of 
+                      # each member to the respective variables
+print(F"({x}, {y}, {z})")
